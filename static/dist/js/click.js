466967280f710ec8a1a799e6223144e83e652534
Tiara Humaira Putri <20102196@ittelkom-pwt.ac.id>
2023-11-04 22:56:30 +0700

    addin static

---
+++ static/dist/js/click.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.alertbutton').forEach(function(button) {
+    button.onclick = function() {
+       var answer = confirm('Hai! Apakah Anda yakin akan menghapus pilihan basis pengetahuan ini?');
+       if (answer) {
+         alert('Basis pengetahuan yang anda pilih berhasil dihapus!');
+       } else {
+         alert('Basis pengetahuan yang anda pilih gagal dihapus!');
+       }
+    }
+});
