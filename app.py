daec5ed0530aa6563605b774d56681de311d96ce
Tiara Humaira Putri <20102196@ittelkom-pwt.ac.id>
2023-11-04 23:08:48 +0700

    addin app.py for admin

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/indexweb")
 def home():
     return render_template('indexweb.html')
 
@@ -18,6 +18,46 @@ def indiag():
 def dtdiag():
     return render_template('indexdata.html')
 
+
+@app.route('/') #index
+def main():
+    return render_template('indexadm.html')
+
+@app.route('/gejala') #index
+def gejala():
+    return render_template('gejaladm.html')
+
+@app.route('/addgejala')
+def addgejala():
+    return render_template('addgejala.html')
+
+@app.route('/updategejala')
+def updategejala():
+    return render_template('updategejala.html')
+
+@app.route('/penyakit')
+def penyakit():
+    return render_template('penyakitadm.html')
+
+@app.route('/addpenyakit')
+def addpenyakit():
+    return render_template('addpenyakit.html')
+
+@app.route('/updatepenyakit')
+def updatepenyakit():
+    return render_template('updatepenyakit.html')
+
+@app.route('/basis')
+def basis():
+    return render_template('basisadm.html')
+
+@app.route('/addbasis')
+def addbasis():
+    return render_template('addbasis.html')
+
+@app.route('/detailbasis')
+def detailbasis():
+    return render_template('detailbasis.html')
 
 
 if __name__ == '__main__':
